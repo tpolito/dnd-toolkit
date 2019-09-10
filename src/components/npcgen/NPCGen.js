ef67130ef4c486312cf0542fb22fd7dafd2cf700
tpolio <0613.tyler@gmail.com>
2019-09-10 12:48:41 -0500

    started data structure and npcgen debug layouts

--- src/components/npcgen/NPCGen.js
+++ src/components/npcgen/NPCGen.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import data from './npcData/data.json';
 
 const NPCGen = () => {
+  // Get a random number
+  let random = Math.floor(Math.random() * 4);
+  // Log a Elf First and Last Name
+  console.log(`${data.elf.mFirstName[random]} ${data.elf.mLastName[random]} `);
   return (
     <div>
       <h1>NPC Name Generator</h1>
