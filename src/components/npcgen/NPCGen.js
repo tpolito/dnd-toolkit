97aacb732bddd39613473cd7e02a5c1d109ffc6e
tpolio <0613.tyler@gmail.com>
2019-09-20 14:24:53 -0500

    added input fields, and constructed new data for the data import

--- src/components/npcgen/NPCGen.js
+++ src/components/npcgen/NPCGen.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './npcData/data.json';
 
 const NPCGen = () => {
+  // State
+  const [gender, setGender] = useState('');
+  const [name, setName] = useState('');
+
+  // Deconstruct
+  const { elf } = data;
+
+  // onChange Handle
+  const onChange = e => {
+    setGender({ ...gender, [e.target.name]: e.target.value });
+  };
+
+  // Set random number interval
+  let random = 0;
   // Get a random number
-  let random = Math.floor(Math.random() * 4);
-  // Log a Elf First and Last Name
-  console.log(
-    `${data.elf.mFirstName[random]} ${data.elf.elfLastName[random]} `
-  );
+  const getRandomNum = num => {
+    random = Math.floor(Math.random() * num);
+  };
+
+  // Get a random name from the data
+  const getRandomName = () => {
+    getRandomNum(4);
+    name = `${elf.fFirstName[random]} ${elf.elfLastName[random]}`;
+
+    console.log(name);
+  };
   return (
     <div>
-      <h1>NPC Name Generator</h1>
-      <button className="waves-effect waves-light btn-large">
-        Generate An NPC
-      </button>
+      <h4 className="center">NPC Name Generator</h4>
+      <div className="center">
+        <div className="input-field">
+          <select
+            name="type"
+            value={gender}
+            className="browser-default"
+            onChange={onChange}
+          >
+            <option value="" disabled>
+              Select Gender of Character
+            </option>
+            <option value="m">Male</option>
+            <option value="f">Female</option>
+          </select>
+        </div>
+        <button
+          onClick={getRandomName}
+          className="waves-effect waves-light btn-large"
+        >
+          Generate An NPC
+        </button>
+        <div>
+          <p>{name}</p>
+        </div>
+      </div>
     </div>
   );
 };
